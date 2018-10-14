675beabd19a56d4ceac7c53659409abacf73edf9
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-10-14 09:46:42 -0500

    add git branch to line

--- profile.sh
+++ profile.sh
@@ -12,6 +12,6 @@ for f in $CUR_DIR/config/**; do source $f; done
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 
 # improve terminal appearance
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[00m\]\$(parse_git_branch)\[\033[00m\] $ "
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
